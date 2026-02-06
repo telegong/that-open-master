7efd87d7f5fa7954bd824989137480bb6ad5114a
Eugene Choi <tnx2u2@gmail.com>
2026-02-07 01:58:35 +0900

    Sidebar.tsx company-logo src path 수정

--- src/react-components/Sidebar.tsx
+++ src/react-components/Sidebar.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 export function Sidebar() {
     return(
         <aside id="sidebar">
-            <img id="company-logo" src="./assets/company-logo.svg" alt="Construction Company" />
+            <img id="company-logo" src="../../assets/company-logo.svg" alt="Construction Company" />
             <ul id="nav-buttons">
                 <li id="projects-list-page-button"><span className="material-icons-round">apartment </span>  Projects</li>
                 <li id="users-list-page-button"><span className="material-icons-round">people </span>  Users</li>
