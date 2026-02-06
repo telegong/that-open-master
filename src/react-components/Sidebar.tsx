033e88491bfa260de5123d53bb14324da9c3163b
Eugene Choi <tnx2u2@gmail.com>
2026-02-07 02:07:14 +0900

    logo path change

--- src/react-components/Sidebar.tsx
+++ src/react-components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import companyLogo from "../../assets/company-logo.svg"
+import companyLogo from "./assets/company-logo.svg"
 
 export function Sidebar() {
     return(
